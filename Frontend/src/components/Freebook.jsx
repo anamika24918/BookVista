fcf3562d324580f470fa5249d914b0077485396d
anamika24918 <120499409+anamika24918@users.noreply.github.com>
2025-01-23 14:31:59 +0530

    Done

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import axios from "axios"
 
-import list from "../../public/list.json"
+// import list from "../../public/list.json"
 import Cards from './Cards';
-
 function Freebook() {
+    const [book, setBook] = useState([]);
+
+    useEffect(() => {
+        const getBook = async () => {
+            try {
+                const res = await axios.get("http://localhost:4001/book");
+                console.log(res.data); // Correctly log the response data
+                const data = res.data.filter((data) => data.category === "Free");
+                setBook(data);
+            } catch (error) {
+                console.error(error); // Use `console.error` for better logging
+            }
+        };
+        getBook();
+    }, []);
+
     var settings = {
         dots: true,
         infinite: false,
@@ -21,46 +37,124 @@ function Freebook() {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     infinite: true,
-                    dots: true
-                }
+                    dots: true,
+                },
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                    initialSlide: 2
-                }
+                    initialSlide: 2,
+                },
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+                    slidesToScroll: 1,
+                },
+            },
+        ],
     };
-    const filterData = list.filter((data) => data.category === "Free");
-    console.log(filterData);
+
+    // Remove the unnecessary `filterData` reference
+    // The filtering is already done inside the `useEffect` above
+
     return (
         <>
             <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
                 <div>
                     <h1 className="font-semibold text-xl pb-2">Free offered courses</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore exercitationem, ut consequatur placeat voluptatibus debitis labore eveniet rerum id officiis culpa laudantium earum velit similique magnam ex quo. Quas, quam!</p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Dolore exercitationem, ut consequatur placeat voluptatibus
+                        debitis labore eveniet rerum id officiis culpa laudantium
+                        earum velit similique magnam ex quo. Quas, quam!
+                    </p>
                 </div>
                 <div>
                     <Slider {...settings}>
-                        {filterData.map((item) => (
+                        {book.map((item) => (
                             <Cards item={item} key={item.id} />
                         ))}
                     </Slider>
                 </div>
             </div>
-
         </>
-    )
+    );
 }
 
-export default Freebook
+export default Freebook;
+// function Freebook() {
+//     const [book, setBook] = useState([])
+//     useEffect(() => {
+//         const getBook = async () => {
+//             try {
+//                 const res = await axios.get("http://localhost:4001/book");
+//                 console.log(data);
+//                 const data = res.data.filter((data) => data.category === "Free");
+//                 setBook(data);
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         }
+//         getBook();
+//     }, []);
+//     var settings = {
+//         dots: true,
+//         infinite: false,
+//         speed: 500,
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         initialSlide: 0,
+//         responsive: [
+//             {
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 3,
+//                     slidesToScroll: 3,
+//                     infinite: true,
+//                     dots: true
+//                 }
+//             },
+//             {
+//                 breakpoint: 600,
+//                 settings: {
+//                     slidesToShow: 2,
+//                     slidesToScroll: 2,
+//                     initialSlide: 2
+//                 }
+//             },
+//             {
+//                 breakpoint: 480,
+//                 settings: {
+//                     slidesToShow: 1,
+//                     slidesToScroll: 1
+//                 }
+//             }
+//         ]
+//     };
+//     // const filterData = list.filter((data) => data.category === "Free");
+//     console.log(filterData);
+//     return (
+//         <>
+//             <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+//                 <div>
+//                     <h1 className="font-semibold text-xl pb-2">Free offered courses</h1>
+//                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore exercitationem, ut consequatur placeat voluptatibus debitis labore eveniet rerum id officiis culpa laudantium earum velit similique magnam ex quo. Quas, quam!</p>
+//                 </div>
+//                 <div>
+//                     <Slider {...settings}>
+//                         {book.map((item) => (
+//                             <Cards item={item} key={item.id} />
+//                         ))}
+//                     </Slider>
+//                 </div>
+//             </div>
+
+//         </>
+//     )
+// }
+
+// export default Freebook
